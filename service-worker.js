ec1fce5ece7669e63374071763ef335f1c706ca1
joaolk017 <joaomota0778@gmail.com>
2026-09-22 17:25:26 -0300

    Atualiza cache do app para remover versões antigas da home

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='cv360-shell-v2';
+const CACHE='cv360-shell-v3';
 const SHELL=['/','/manifest.webmanifest','/pwa-icon.svg','/file_000000009280820e99defdb780777d3a.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
